fb2ecb80aed0274ad65730f703f1e0c9247917f8
Vincent Jacquier <vin.jacquier@gmail.com>
2015-05-01 23:44:03 +0200

    add move

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: vjacquie <vjacquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/01 23:31:06 by vjacquie          #+#    #+#             */
-/*   Updated: 2015/05/01 23:41:32 by vjacquie         ###   ########.fr       */
+/*   Updated: 2015/05/01 23:44:01 by vjacquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void 	move(t_data *d, int move)
 {
-	if (move == LEFT && d->player->pos > 2)
+	if (move == LEFT && d->player_pos > 2)
 	{
 		d->map[MAP_Y - 1][d->player_pos + 1].type = 5;
 		d->player->pos--;
 		d->map[MAP_Y - 1][d->player_pos - 1].type = 6;
 	}
-	else if (move == RIGHT && d->player->pos < MAP_X - 2)
+	else if (move == RIGHT && d->player_pos < MAP_X - 2)
 	{
 		d->map[MAP_Y - 1][d->player_pos - 1].type = 5;
 		d->player->pos++;
