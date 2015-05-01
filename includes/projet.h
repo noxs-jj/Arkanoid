8a81b74623e36c00ab4097879716154c74bd256b
Vincent Jacquier <vin.jacquier@gmail.com>
2015-05-01 23:43:20 +0200

    add move

--- includes/projet.h
+++ includes/projet.h
@@ -7,10 +7,13 @@
 # define MAP_X 50
 # define MAP_Y 45
 
-# define WALL '#'
-# define SIMPLE '-'
-# define OBSTACLE '@'
-# define EMPTY ' '
+# define WALL '#'		// type 1
+# define SIMPLE '-'		// type 2
+# define OBSTACLE '@'	// type 3
+# define EMPTY ' '		// type 5
+# define PLAYER '_'		// type 6
+# define LEFT 1
+# define RIGHT 2
 
 typedef struct s_case
 {
@@ -27,6 +30,7 @@ typedef struct		s_data
 	int 			fd_level;
 	int 			level;
 	t_case			**map;
+	int 			player_pos;
 
 	// DEBUGG for dev can be bonus
 	unsigned short int	op_log;
@@ -41,5 +45,6 @@ void				w_log(char const *str);
 int		get_level_file(t_data *d);
 int		fill_map(t_data *d);
 int		init_map(t_data *d);
+void 	move(t_data *d, int move);
 
 #endif
